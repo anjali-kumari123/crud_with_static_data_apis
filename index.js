922703d72ffeff6896c410386efdcc84c17b929a
Anjali Kumari <anjalikumari.edugaon@gmail.com>
2026-02-17 10:53:56 +0530

    i worked on delete student

--- index.js
+++ index.js
@@ -43,6 +43,23 @@ app.get('/getAllStudents',(request,response)=>{
 
   });
 });
+
+  app.delete("/deleteStudent/:id",(req,res)=>{
+    const sId=parseInt(req.params.id);
+    const studentIndex=students.findIndex(o=>o.id===sId);
+
+    if(studentIndex===-1){
+        res.json({
+            message:"user not found"
+        });
+    }
+    const deleteStudent=students.splice(studentIndex,1);
+    res.json({
+      message:"order deleted successfully",
+      deleteData:deleteStudent[0]
+    });
+  });
+
 app.listen(3000,()=>{
   console.log("server is running");
 })
